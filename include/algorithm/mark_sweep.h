a4db3792f23c6e95be8249c3c6dc690a284f973d
liancheng <rhythm.mail@gmail.com>
2010-08-14 05:46:39 +0000

    - Fix issue 1 (https://code.google.com/p/cppgc/issues/detail?id=1)
    - Code style partially adjusted.

--- include/algorithm/mark_sweep.h
+++ include/algorithm/mark_sweep.h
@@ -1,280 +1,330 @@
 #ifndef __MARK_SWEEP_H__
 #define __MARK_SWEEP_H__
-#include <new>
-#include <cstdlib>
-#include <set>
-#include <stack>
-#include <algorithm>
+
 #include "thread/single_thread.h"
-#include "trigger/trigger_common.h"
 #include "trigger/manual.h"
-namespace windreamer
+#include "trigger/trigger_common.h"
+
+#include <algorithm>
+#include <cstdlib>
+#include <deque>
+#include <new>
+#include <set>
+
+namespace windreamer {
+namespace gc {
+namespace algorithm {
+
+template <
+    typename Trigger = triggers::Manual,
+    template<typename> class Allocator = std::allocator,
+    typename ThreadingModel = thread::policy::SingleThread
+>
+struct MarkSweep
 {
-	namespace gc
-	{
-		namespace algorithm
-		{
-			template <typename Trigger=triggers::Manual,template<typename> class Allocator=std::allocator, typename ThreadingModel=thread::policy::SingleThread>
-			struct MarkSweep
-			{
-				typedef typename ThreadingModel::Lock Lock;
-				struct HandleBase;
+#ifdef __x86_64__
+    typedef unsigned long AddressType;
+#else
+    typedef unsigned int AddressType;
+#endif
 
-				struct Controller
-				{
+    typedef std::set<AddressType> AddressSet;
 
-					static std::set<int> getRootSet()
-					{
-						std::set<int> result;
-						std::set<int>::const_iterator begin=HandleBase::getRegistry().begin();
-						std::set<int>::const_iterator end=HandleBase::getRegistry().end();
-						std::remove_copy_if(begin,end,std::insert_iterator<std::set<int> >(result,result.begin()),isNotRoot);
-						return result;
-					}
-					static int getWrapperCount()
-					{
-						return WrapperBase::getRegistry().size();
-					}
-					static void forceGC()
-					{
-						mark();
-						sweep();
-						reset();
-					}
-				private:
-					static bool isNotRoot(int i)
-					{
-						return !((reinterpret_cast<HandleBase*>(i))->isRoot());
-					}
+    typedef typename ThreadingModel::Lock Lock;
 
-					static void mark()
-					{
-						std::stack<int> s;
-						std::set<int> roots=getRootSet();
-						for (std::set<int>::const_iterator iter=roots.begin();iter!=roots.end();++iter)
-						{
-							s.push(*iter);
-						}
-						while(!s.empty())
-						{
-							int address=s.top();
-							s.pop();
-							HandleBase* ptr=reinterpret_cast<HandleBase*>(address);
-							ptr->ptr->alive=true;
-							std::set<int> members=ptr->getMemberHandles();
-							for (std::set<int>::const_iterator iter=members.begin();iter!=members.end();++iter)
-							{
-								if (!reinterpret_cast<HandleBase*>(*iter)->ptr->alive)
-								{
-									s.push(*iter);
-								}
-							}
+    struct HandleBase;
+    struct WrapperBase;
 
-						}
+    struct Controller
+    {
 
+        static AddressSet getRootSet()
+        {
+            AddressSet result;
 
-					}
+            std::remove_copy_if(
+                    HandleBase::getRegistry().begin(),
+                    HandleBase::getRegistry().end(),
+                    std::inserter(result, result.begin()),
+                    isNotRoot);
 
-					static void sweep()
-					{
-						for (std::set<int>::const_iterator it = WrapperBase::getRegistry().begin(); it != WrapperBase::getRegistry().end();)
-						{
-							WrapperBase* ptr=reinterpret_cast<WrapperBase*>(*it);
-							it++;
-							WrapperBase::check_delete(ptr);
-						}
+            return result;
+        }
 
-					}
-					static void reset()
-					{
-						std::for_each(WrapperBase::getRegistry().begin(),WrapperBase::getRegistry().end(),resetAlive);
-					}
+        static int getWrapperCount()
+        {
+            return WrapperBase::getRegistry().size();
+        }
 
-					static void destroyDead(int p)
-					{
-						WrapperBase* ptr=reinterpret_cast<WrapperBase*>(p);
-						if(!ptr->alive)	delete ptr;
-					}
+        static void forceGC()
+        {
+            mark();
+            sweep();
+            reset();
+        }
 
-					static void resetAlive(int p)
-					{
-						WrapperBase* ptr=reinterpret_cast<WrapperBase*>(p);
-						ptr->alive=false;
-					}
+    private:
+        static bool isNotRoot(AddressType i)
+        {
+            return !(reinterpret_cast<HandleBase*>(i)->isRoot());
+        }
 
-				};
+        static void mark()
+        {
+            std::deque<AddressType> stack;
+            AddressSet roots = getRootSet();
 
-				template <typename U>
-				static Allocator<U> makeAllocator()
-				{
-					return Allocator<U>();
-				}
-				template<triggers::TRIGGER_POINT t>
-				static void trigger()
-				{
-					if (Trigger::test(t))
-					{
-						Controller::forceGC();
-					}
-				}
+            std::copy(roots.begin(), roots.end(), std::front_inserter(stack));
 
-				template <typename T>
-				struct Registry
-				{
-					void insert(T* t)
-					{
-						Lock lock;
-						void* ptr=t;
-						s.insert(reinterpret_cast<int>(ptr));
-					}
+            while(!stack.empty())
+            {
+                AddressType address = stack.front();
+                stack.pop_front();
 
-					void erase(T* t)
-					{
-						Lock lock;
-						void* ptr=t;
-						s.erase(reinterpret_cast<int>(ptr));
-					}
+                HandleBase* ptr = reinterpret_cast<HandleBase*>(address);
+                ptr->ptr->alive = true;
 
-					std::set<int>::const_iterator upper_bound(const void* ptr)
-					{
-						return s.upper_bound(reinterpret_cast<int>(ptr));
-					}
+                AddressSet members = ptr->getMemberHandles();
 
+                for (AddressSet::const_iterator iter = members.begin(); iter != members.end(); ++iter)
+                {
+                    if (!reinterpret_cast<HandleBase*>(*iter)->ptr->alive)
+                    {
+                        stack.push_front(*iter);
+                    }
+                }
+            }
+        }
 
-					std::set<int>::const_iterator lower_bound(const void* ptr)
-					{
-						return s.lower_bound(reinterpret_cast<int>(ptr));
-					}
+        static void sweep()
+        {
+            for (AddressSet::const_iterator it = WrapperBase::getRegistry().begin();
+                    it != WrapperBase::getRegistry().end();)
+            {
+                WrapperBase* ptr = reinterpret_cast<WrapperBase*>(*it);
+                it++;
+                WrapperBase::check_delete(ptr);
+            }
+        }
 
-					std::set<int>::const_iterator begin()
-					{
-						return s.begin();
-					}
+        static void reset()
+        {
+            std::for_each(WrapperBase::getRegistry().begin(), WrapperBase::getRegistry().end(), resetAlive);
+        }
 
-					std::set<int>::const_iterator end()
-					{
-						return s.end();
-					}
+        static void destroyDead(AddressType p)
+        {
+            WrapperBase* ptr = reinterpret_cast<WrapperBase*>(p);
+            if (!ptr->alive)
+                delete ptr;
+        }
 
-					int size()
-					{
-						return s.size();
-					}
-				private:
-					std::set<int> s;
-				};
+        static void resetAlive(AddressType p)
+        {
+            reinterpret_cast<WrapperBase*>(p)->alive = false;
+        }
 
-				struct WrapperBase
-				{
-					WrapperBase()
-						:alive(false)
-					{
-						Registry<WrapperBase>& reg=getRegistry();
-						reg.insert(this);
-					}
-					virtual ~WrapperBase()
-					{
-						Registry<WrapperBase>& reg=getRegistry();
-						reg.erase(this);
-					}
-				protected:
-					static void check_delete(WrapperBase * ptr)
-					{
-						if (!ptr->alive)
-						{
-							delete ptr;
-						}
-					}
-				private:
-					bool alive;
-					friend struct HandleBase;
-					friend struct Controller;
-					static Registry<WrapperBase>& getRegistry()
-					{
-						static Registry<WrapperBase> reg;
-						return reg;
-					}
-					virtual size_t getSize()=0;
-				};
+    };  //  struct Controller
 
-				struct HandleBase{
-					HandleBase(WrapperBase* p=0)
-						:ptr(p),root(-1)
-					{
-						Registry<HandleBase>& reg=getRegistry();
-						static int temp(std::atexit(Controller::forceGC));
-						reg.insert(this);
-					}
-					virtual ~HandleBase()
-					{
-						Registry<HandleBase>& reg=getRegistry();
-						reg.erase(this);
-						ptr=0;
-					}
-					HandleBase(const HandleBase& rhs)
-						:ptr(0),root(-1)
-					{
-						copy(rhs);
-					}
-					bool operator==(const HandleBase& rhs) const
-					{
-						return ptr==rhs.ptr;
-					}
-					bool isRoot() const
-					{
-						if(root==-1)
-						{
-							int size=0;
-							Registry<WrapperBase>& reg=WrapperBase::getRegistry();
-							int nthis=reinterpret_cast<int>(this);
-							std::set<int>::const_iterator i=reg.upper_bound(this);
-							if (i==reg.begin())
-							{
-								root=1;
-								return true;
-							} 
-							else
-							{
-								int address=*(--i);
-								WrapperBase* p=reinterpret_cast<WrapperBase*>(address);
-								size=p->getSize();
-								bool result= nthis>(address+size);
-								root=result;
-								return result;
-							}
-						}
-						return (root!=0);
-					}
+    template <typename U>
+    static Allocator<U> makeAllocator()
+    {
+        return Allocator<U>();
+    }
 
-					std::set<int> getMemberHandles() const
-					{
-						int address=reinterpret_cast<int>(ptr);
-						int size=ptr->getSize();
-						std::set<int> result;
-						std::set<int>::const_iterator begin=getRegistry().lower_bound(ptr);
-						std::set<int>::const_iterator end=getRegistry().upper_bound(reinterpret_cast<void*>(address+size));
-						std::copy(begin,end,std::insert_iterator<std::set<int> >(result,result.begin()));
-						return result;
-					}
+    template<triggers::TRIGGER_POINT t>
+    static void trigger()
+    {
+        if (Trigger::test(t))
+        {
+            Controller::forceGC();
+        }
+    }
 
-				protected:
-					void copy(const HandleBase & rhs)
-					{
-						ptr=rhs.ptr;
-					}
-					WrapperBase* ptr;
-				private:
-					mutable int root;
-					static Registry<HandleBase>& getRegistry()
-					{
-						static Registry<HandleBase> reg;
-						return reg;
-					}
-					friend struct Controller;
-				};
-			private:
-			};
-		}
-	}
-}
+    template <typename T>
+    struct Registry
+    {
+        void insert(T* t)
+        {
+            Lock lock;
+            void* ptr = t;
+            s.insert(reinterpret_cast<AddressType>(ptr));
+        }
+
+        void erase(T* t)
+        {
+            Lock lock;
+            void* ptr=t;
+            s.erase(reinterpret_cast<AddressType>(ptr));
+        }
+
+        AddressSet::const_iterator upper_bound(const void* ptr)
+        {
+            return s.upper_bound(reinterpret_cast<AddressType>(ptr));
+        }
+
+        AddressSet::const_iterator lower_bound(const void* ptr)
+        {
+            return s.lower_bound(reinterpret_cast<AddressType>(ptr));
+        }
+
+        AddressSet::const_iterator begin()
+        {
+            return s.begin();
+        }
+
+        AddressSet::const_iterator end()
+        {
+            return s.end();
+        }
+
+        std::size_t size()
+        {
+            return s.size();
+        }
+
+    private:
+        AddressSet s;
+
+    };
+
+    struct WrapperBase
+    {
+        WrapperBase()
+            :alive(false)
+        {
+            Registry<WrapperBase>& reg = getRegistry();
+            reg.insert(this);
+        }
+
+        virtual ~WrapperBase()
+        {
+            Registry<WrapperBase>& reg = getRegistry();
+            reg.erase(this);
+        }
+
+    protected:
+        static void check_delete(WrapperBase* ptr)
+        {
+            if (!ptr->alive)
+            {
+                delete ptr;
+            }
+        }
+
+    private:
+        bool alive;
+
+        friend struct HandleBase;
+        friend struct Controller;
+
+        static Registry<WrapperBase>& getRegistry()
+        {
+            static Registry<WrapperBase> reg;
+            return reg;
+        }
+
+        virtual size_t getSize() = 0;
+
+    };
+
+    struct HandleBase{
+        HandleBase(WrapperBase* p = 0) :
+            ptr(p),
+            root(-1)
+        {
+            Registry<HandleBase>& reg = getRegistry();
+            static int setup_atexit(std::atexit(Controller::forceGC));
+            reg.insert(this);
+        }
+
+        virtual ~HandleBase()
+        {
+            Registry<HandleBase>& reg = getRegistry();
+            reg.erase(this);
+            ptr = 0;
+        }
+
+        HandleBase(const HandleBase& rhs) :
+            ptr(0),
+            root(-1)
+        {
+            copy(rhs);
+        }
+
+        bool operator==(const HandleBase& rhs) const
+        {
+            return ptr == rhs.ptr;
+        }
+
+        bool isRoot() const
+        {
+            if (root == -1)
+            {
+                int size = 0;
+                Registry<WrapperBase>& reg = WrapperBase::getRegistry();
+                AddressType nthis = reinterpret_cast<AddressType>(this);
+
+                AddressSet::const_iterator i = reg.upper_bound(this);
+                if (i == reg.begin())
+                {
+                    root = 1;
+                    return true;
+                } 
+                else
+                {
+                    AddressType address = *(--i);
+                    WrapperBase* p = reinterpret_cast<WrapperBase*>(address);
+                    size = p->getSize();
+                    bool result = nthis > (address + size);
+                    root = result;
+                    return result;
+                }
+            }
+
+            return (root != 0);
+        }
+
+        AddressSet getMemberHandles() const
+        {
+            AddressType address = reinterpret_cast<AddressType>(ptr);
+            std::size_t size = ptr->getSize();
+
+            AddressSet result;
+
+            std::copy(
+                    getRegistry().lower_bound(ptr),
+                    getRegistry().upper_bound(reinterpret_cast<void*>(address + size)),
+                    std::inserter(result, result.begin()));
+
+            return result;
+        }
+
+    protected:
+        void copy(const HandleBase & rhs)
+        {
+            ptr = rhs.ptr;
+        }
+
+        WrapperBase* ptr;
+
+    private:
+        mutable int root;
+
+        static Registry<HandleBase>& getRegistry()
+        {
+            static Registry<HandleBase> reg;
+            return reg;
+        }
+
+        friend struct Controller;
+
+    };  //  struct HandleBase
+
+};  //  struct MarkSweep
+
+}   //  namespace algorithm
+}   //  namespace gc
+}   //  namespace windreamer
+
 #endif//__MARK_SWEEP_H__
